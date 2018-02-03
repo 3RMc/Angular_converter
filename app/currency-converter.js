b19a2c372e6cf26938e615bbf004cdf9e4b19b7c
3RMc <ermakoff1994@mail.ru>
2018-02-03 17:57:37 +0300

    Контроллер с переменными хранения данных о валютах

--- app/currency-converter.js
+++ app/currency-converter.js
@@ -5,4 +5,15 @@ angular.module('currencyConverter').directive('currencyConverter', function () {
         controllerAs: 'curConvCtrl',
         templateUrl: 'currency-converter.html'
     }
-})
+});
+
+angular.module('currencyConverter').controller('CurConvCtrl', CurConvCtrl);
+
+function CurConvCtrl($scope, CurrencyService) {
+    this.currencies = [];      // массив для хранения валют
+    this.currencyIn = null;    // валюта на руках
+    this.currencyOut = null;   // валюта в обмен
+    this.currencyRates = [];   // коэффициенты валют
+    this.sumIn = 1;            // сумма на входе
+    this.sumOut = 0;           // сумма на выходе
+}
